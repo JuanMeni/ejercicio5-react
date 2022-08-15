d77f2afe9c4edd5cf48c8d8b7c1a8d7801985694
Juan Menichetti <jpmenichetti13@gmail.com>
2022-08-14 21:08:40 -0300

    se agrego la hoja de estilo de bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Formulario from './components/Formulario';
 import Subtitulo from './components/Subtitulo';
 import Titulo from './components/Titulo';
